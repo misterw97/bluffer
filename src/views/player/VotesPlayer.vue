4aa49307eacde6cfdabb99e2fa528d1fb974d211
rogerxaic <roger.miret@gmail.com>
2020-05-02 23:21:50 +0000

    Add VotesPlayer view

--- src/views/player/VotesPlayer.vue
+++ src/views/player/VotesPlayer.vue
@@ -1,41 +1,49 @@
 <template>
   <div class="votes">
-    <h2>Question</h2>
-    <textarea name id cols="30" rows="3"></textarea>
+    <Answer :disabled="true" :value="game.data.question" title="Question"></Answer>
 
     <h2>
       Devine la bonne réponse !
       <small>(Tu ne peux pas voter pour la tienne)</small>
     </h2>
     <div class="responses">
-      <Button @click="vote(response)" class="response" :disabled="response.disabled" v-for="response in responses" :key="response.id">{{response.title}}</Button>
+      <div v-for="response in responses" :key="response.id" class="response">
+        <Button @click="vote(response)" :disabled="response.disabled">{{response.title}}</Button>
+        <Avatar v-if="voted === response.id" :size="35" :player="player" />
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
+import Answer from "../../components/Answer.vue";
+import Avatar from "../../components/Avatar.vue";
 import Button from "../../components/Button.vue";
 import Game from "../../models/Game";
+import Player from "../../models/Player";
 
 @Component({
   components: {
+    Answer,
+    Avatar,
     Button
   }
 })
 export default class extends Vue {
   @Prop() private game!: Game;
+  @Prop() private player!: Player;
 
-  private _voted?: number;
+  private voted: number = -1;
 
   responses = [
     { id: 1, title: "Réponse 1, credible mais bon, on a quand même un doute" },
-    { id: 2, title: "Réponse 2, la mienne", disabled: true, },
+    { id: 2, title: "Réponse 2, la mienne", disabled: true },
     { id: 3, title: "Réponse 3, la vraie" }
   ];
 
-  vote(data: any) {
-    console.log(data);
+  vote(response: any) {
+    this.voted = response.id;
   }
 }
 </script>
@@ -53,6 +61,18 @@ $margin: 10px;
       width: calc(50% - #{2 * $margin});
       margin: $margin;
       font-size: 1.17em;
+      position: relative;
+      display: flex;
+      align-items: center;
+
+      img {
+        $size: 35px;
+        position: absolute;
+        bottom: #{$size / -2};
+        height: $size;
+        right: #{$size / 2};
+        border: 1px solid white;
+      }
     }
   }
 }
