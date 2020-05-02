f270e0885aeef9ca06e7c534323493e7c5e4096e
rogerxaic <roger.miret@gmail.com>
2020-05-02 15:58:30 +0000

    Combine package.json

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const { Game } = require('./game');
@@ -27,9 +28,7 @@ const createPlayer = () => {
   return playerId;
 }
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+app.use(express.static(__dirname + '/../dist'));
 
 io.on('connection', (socket) => {
   console.log(`a user connected: ${socket.id}`);
