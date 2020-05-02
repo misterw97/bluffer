f6d3d28433857f22d451823a7bcf52ee1f7e551b
Valentin Viennot <misterw97@users.noreply.github.com>
2020-05-02 18:04:26 +0200

    Style welcome page

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -5,30 +5,32 @@
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
+import { Component, Prop, Vue } from "vue-property-decorator";
 
 @Component
 export default class HelloWorld extends Vue {
   myMethod() {
-    this.$emit('click');
+    this.$emit("click");
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
- .button {
+.button {
+  width: 100%;
+  height: 100%;
   background-color: $primary;
-    padding: 10px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    color: white;
-    font-weight: bold;
-    // border: 5px solid $secondary;
-    border-radius: 6px;
-    &:hover {
-      cursor: pointer;
-    }
- }
+  padding: 20px 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  font-weight: bold;
+  border: 3px solid $secondary;
+  border-radius: 15px;
+  &:hover {
+    cursor: pointer;
+  }
+}
 </style>
