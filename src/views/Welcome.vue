c3ab5b9ec42ce7071e002beb551ee9a16aab4089
rogerxaic <roger.miret@gmail.com>
2020-05-02 18:54:05 +0000

    join game from frontend

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -5,7 +5,7 @@
     <h2>Quel est ton prénom?</h2>
     <input type="text" v-model="name" placeholder="Jean-Pierre" />
     <Button v-if="!join" @click="createNewGame()">Créer un nouveau jeu</Button>
-    <Button v-if="!join" @click="joinExistingGame()" >Rejoindre une partie</Button>
+    <Button v-if="!join" @click="joinExistingGame()">Rejoindre une partie</Button>
 
     <h2 v-if="join">Quel est le code de la partie?</h2>
     <input v-if="join" type="text" v-model="code" placeholder="123456" />
@@ -26,6 +26,7 @@ import { Socket } from "vue-socket.io-extended";
 export default class Welcome extends Vue {
   private join = false;
   private name = "";
+  private code = "";
 
   @Socket() // --> listens to the event by method name, e.g. `connect`
   connect() {
@@ -34,35 +35,33 @@ export default class Welcome extends Vue {
 
   joinExistingGame() {
     if (this.join) {
-      //socker.send join
+      this.joinGame({ name: this.name, game: this.code });
     } else {
       this.join = true;
     }
   }
 
   createNewGame() {
-    this.$socket.client.emit(
-      "join",
-      {
-        name: this.name
-      },
-      (data: any) => {
-        this.$router.push({
-          name: "Game",
-          params: {
-            player: data.id,
-            game: data.game
-          }
-        });
-      }
-    );
+    this.joinGame({ name: this.name });
+  }
+
+  joinGame(value: any) {
+    this.$socket.client.emit("join", value, (data: any) => {
+      this.$router.push({
+        name: "Game",
+        params: {
+          player: data.id,
+          game: data.game
+        }
+      });
+    });
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
-.hello {  
+.hello {
   display: flex;
   flex-direction: column;
   align-items: flex-start;
@@ -70,7 +69,8 @@ export default class Welcome extends Vue {
   margin: 15vh auto auto;
 
   .button,
-  input, h2 {
+  input,
+  h2 {
     margin-top: 20px;
   }
 
@@ -84,5 +84,4 @@ export default class Welcome extends Vue {
     width: 100%;
   }
 }
-
 </style>
