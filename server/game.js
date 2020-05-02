a61868454ba6ef5e4a37f6fcce971d60527adabc
Valentin Viennot <misterw97@users.noreply.github.com>
2020-05-02 20:17:49 +0200

    fix bug with counter init

--- server/game.js
+++ server/game.js
@@ -18,7 +18,7 @@ class Game {
     }
 
     _newState() {
-        const lastCount = this.state?.count || 0;
+        const lastCount = (this.state ? this.state.count : undefined) || 0;
         this.state = {
             state: State.question,
             count: lastCount + 1,
